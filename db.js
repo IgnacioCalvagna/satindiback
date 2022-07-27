b6e34fa053a5505bb31d66a0e228d0c4423b7d3e
Ignacio Calvagna <ignaciocalvagna@gmail.com>
2022-07-27 16:28:39 -0300

    asdasfa

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize");
 const { Pool } = require("pg");
+const conectionString = process.env.DATABASE_URL 
 const pool = new Pool({ conectionString });
 
 const db = new Sequelize("satindiback", null, null, {
@@ -8,6 +9,6 @@ const db = new Sequelize("satindiback", null, null, {
   logging: false,
 });
 
-const conectionString = process.env.DATABASE_URL 
+
 
 module.exports = {db,pool};
