993e0f6485827dd58b27740d18897afad74f1bf8
Ignacio Calvagna <ignaciocalvagna@gmail.com>
2022-07-28 10:14:24 -0300

    dasfa

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const  {User}  = require("./models");
+const  User  = require("./models/User");
 const app = express();
 const db = require("./db");
 const port = process.env.PORT || 3001;
